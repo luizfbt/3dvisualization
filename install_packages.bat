5dd98782202281dc4aa418c19a127e799824192d
luizfbt <brumteixeira@gmail.com>
2024-10-17 11:11:55 -0300

    Create install_packages.bat

---
+++ install_packages.bat
@@ -0,0 +1,16 @@
+call python.exe -m pip install --upgrade pip
+call pip install --upgrade setuptools wheel
+call pip install --upgrade numpy
+call pip install --upgrade pandas
+call pip install --upgrade jupyterlab
+call pip install --upgrade pprintpp
+call pip install --upgrade flet
+call pip install --upgrade vpython
+call pip install --upgrade pymesh
+call pip install --upgrade vtk
+call pip install --upgrade trame
+call pip install --upgrade trame_vtk
+call pip install --upgrade trame-vuetify
+call pip install --upgrade pyvista
+call pip install --upgrade pyntcloud
+call pip install --upgrade sympy
